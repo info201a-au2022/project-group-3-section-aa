70dc3af50575e6bb48897224ae14ce143cf47187
Zhi Huang <zhihuang@uw.edu>
2022-12-07 21:48:51 -0800

    word count

--- source/conclusion.R
+++ source/conclusion.R
@@ -29,16 +29,28 @@ conclusion <- fluidPage(
   cancer. This will give people more visual information and allow users to participate in the comparison of pictures
   to understand and check the factors of cancer and thus reduce the mortality rate of cancer."),
   
-  h2("Report"),
+  h2("Conclusion"),
   
-  p("In conclusion, these data are use to understand cancer population growth, cancer burden and trends,
-     support cancer research, mesure progress in cancer control, environmental factors encorcement, mesure
-    cancer prevention efforts, target action on eliminating disparities, and improve cancer outcomes for all.
-    The interative data visualizations tool makes it easy for people to access to and check the latest update
-    of cancer related informations, making resources easy for anyone to explore and use. The map visualizations
-    allow us to view rates of cancers or cancer mortality for the entire United States and individual states. Our
-    demographics charts allow people to see rates or numbers of cancers groth by sex, and age group for all cancer
-    types combined for 27 types of cancers. Additionally, we generated county-level model-based estimates for 4 
-    cancer risk factors. The scatter visuliazation give a sense of how different potential risk
-    factors affect people's health conditions over time. ")
+  p("Cancer, a disease in which abnormal cells divide uncontrollably and destroy body tissue, is the second leading 
+  cause of death in the United States, exceeded only by heart disease. One of every five deaths in the United States 
+  is due to cancer. Thanks to the development of medical care, hospitalization, research findings, and technology, the 
+  survival rate of many types of cancers are improving. 
+  
+  To conclude our project, we want to reiterate some of our key findings. First and foremost, our data revels that age 
+  increase is the major factor that lead to cancer, and it is considered as an unavoidable factor. As we dive deeper, 
+  we find out that female are at a 0.15%, a slightly higher risk at getting caner. As we move along and learn about the 
+  27 types of cancer from our data, we discovered that Breast and Lung cancers were the most commonly developed cancers 
+  in the U.S., with an increase of 12% of total number of new cases. Our interactive data visualizations tool makes it easy 
+  for people to access to and check the latest update of cancer related informations, making resources easy for anyone to 
+  explore and use. The map visualizations allow viewers to see rates of cancers or cancer mortality for the entire United States 
+  and individual states. The interactive bar chart allow viewers to select different states for details, and the colors differentiate 
+  different levels of rate for a better. Our demographics charts allow people to see rates or numbers of cancers growth by sex, and 
+  age group for all cancer types combined for 27 types of cancers. According to CDC, the rates of cancer diagnoses and cancer deaths are 
+  impacted by changes in exposure to risk factors. Took that into consideration, we generated county-level model-based estimates for 4 
+  cancer risk factors. The scatter plot visualization give a sense of how different potential risk factors affect people's health 
+  conditions over time.
+
+  We hope these data can be used use to understand cancer population growth, cancer burden and trends, support cancer research, 
+  measure progress in cancer control, environmental factors enforcement, measure cancer prevention efforts, target action on 
+  eliminating disparities, and improve cancer outcomes for all. "),
 )
